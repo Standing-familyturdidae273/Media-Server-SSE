f5651047608a8a4402bc0658b0ce2a94f73aea26
Connor Gallopo <connor.gallopo@me.com>
2026-03-31 18:15:47 -0400

    add emby plugin entry point

---
+++ Emby.Plugin.Sse/SsePlugin.cs
@@ -0,0 +1,22 @@
+using System;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace Emby.Plugin.Sse
+{
+    public class SsePlugin : BasePlugin<BasePluginConfiguration>
+    {
+        public SsePlugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+        }
+
+        public override Guid Id => new Guid("A3D8F1E6-2B7C-4E9A-8F5D-1C6B0A3E7F92");
+
+        public override string Name => "SSE";
+
+        public override string Description => "Server-Sent Events for real-time playback and session notifications.";
+    }
+}
